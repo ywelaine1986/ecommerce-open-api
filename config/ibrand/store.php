d0bffbafaaa05c1bbe86d28aa886f931c5c05f65
Yuwen <yuwen@thesolong.com>
2019-09-24 22:06:53 +0800

    add generated files

---
+++ config/ibrand/store.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2016/3/29
+ * Time: 16:35
+ */
+return [
+    'color' => [
+        '黑色',
+        '绿色',
+        '白色',
+        '紫色',
+        '红色',
+        '黄色',
+        '蓝色',
+        '棕色',
+        '灰色'
+    ],
+
+    'page_title' => env('BACKEND_PAGE_TITLE', 'ibrand后台管理系统'),
+
+    'short_title' => env('BACKEND_PAGE_TITLE', 'ibrand'),
+
+];
